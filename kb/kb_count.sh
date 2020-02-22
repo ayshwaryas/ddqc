dc4a8c80391ca72d6b041e5b9a31041df6d91505
Michael Alperovich <misha.alperovich1@gmail.com>
2020-02-22 11:56:03 -0500

    kb scripts fixes

--- kb/kb_count.sh
+++ kb/kb_count.sh
@@ -5,8 +5,9 @@ reuse R-3.5
 reuse Anaconda3
 source activate /ahg/regevdata/projects/scqc/myenv2
 
-cd /broad/hptmp/malperov/kb_test/
+cd /broad/hptmp/malperov/data/kb_test/
 cd $1
 
 kb count -i ../Mus_musculus.GRCm38.cdna.all.idx -g ../transcripts_to_genes.txt -x 10xv2 -t 2 *.fastq.gz
-R /home/unix/malperov/method_comparison/kb/convert.R $1
+
+Rscript /home/unix/malperov/method_comparison/kb/convert.R $1
