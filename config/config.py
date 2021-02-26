011153c94ab871ccab3a304681d3b63826015c42
Michael Alperovich <misha.alperovich1@gmail.com>
2021-02-26 18:55:52 -0500

    config changes and refactored mc script

--- config/config.py
+++ config/config.py
@@ -1,6 +1,9 @@
 from local_config import local
 
 
+LOCAL = local  # needed for running outside the cluster
+
+# PATHS
 if not local:
     DATA_DIR = "/ahg/regevdata/projects/scqc/data/"  # directory with all data
     OUTPUT_DIR = "/ahg/regevdata/projects/scqc/output_pg/"  # output directory
@@ -15,12 +18,12 @@ else:  # for debug outside of cluster
         DATA_DIR = "/Volumes/scqc/data/"
     OUTPUT_DIR = "/Users/michaelalperovich/Documents/primes_storage/output_pg/"
 
-SOURCE_DIR_PREFIX = OUTPUT_DIR  # for fc plots, do not change
-
-basic_genes_filter = 100  # basic nGenes filter (performed for all methods)
-basic_mito_filter = 80
+# CLUSTERING
 resolution = 1.4  # this resolution gives results closest to seurat
 
+# FILTERING
+basic_genes_filter = 100  # basic nGenes filter (performed for all methods)
+basic_mito_filter = 80
 mito_prefixes = {"human": "MT-", "mouse": "Mt-"}  # prefixes of mitochondrial genes
 # TODO: check ribo prefix
 ribo_prefixes = {"human": "^Rp[sl]\d", "mouse": "^Rp[sl]\d"}  # prefixes of ribosomal genes
@@ -30,3 +33,8 @@ do_counts = True
 do_genes = True
 do_mito = True
 do_ribo = True
+
+# METHOD COMPARISON SCRIPTS
+# none - no additional filtering; cutoff - min 200 genes, max 10% mito; outlier and mad - data driven methods
+MC_METHODS = (("none", 0), ("cutoff", 10), ("mad", 2))
+MC_TASKS_PER_TISSUE = len(MC_METHODS)
---
+++ method_comparison.py
@@ -0,0 +1,65 @@
+import subprocess
+import sys
+
+from config.config import *
+from filtering import filter_cells
+from reading import read_tissue, get_project_info
+from utils import cluster_data, safe_mkdir, add_cd_scores, write_markers, save_to_csv
+
+
+# function that creates all the relevant directories
+def create_dirs(project, tissue, res, method, param):
+    task_directory = "{}-{}-{}".format(res, param, method)  # name of the directory for this task
+    task_name = tissue + "-" + task_directory  # task name to put on plots
+    results_dir = OUTPUT_DIR + project + "/" + tissue + "/" + task_directory + "/"  # directory for saving output
+
+    # create all the directories, if they dont exist
+    print("Creating Output Directories")
+    safe_mkdir(OUTPUT_DIR)
+    safe_mkdir(OUTPUT_DIR + project + "/")
+    safe_mkdir(OUTPUT_DIR + project + "/" + tissue)
+    safe_mkdir(results_dir)
+
+    return task_directory, task_name, results_dir
+
+
+def mc_main(project, task_id, tissue=None):
+    if tissue is None:
+        tissue, is_human, annotations = get_project_info(project, task_id=task_id // MC_TASKS_PER_TISSUE)
+    else:
+        is_human, annotations = get_project_info(project, tissue=tissue)
+
+    adata = read_tissue(project, tissue)
+
+    method, param = MC_METHODS[task_id % MC_TASKS_PER_TISSUE]
+    print(
+        "task.id:{} - tissue:{}, res:{}, method:{}, param:{}, project:{}, do.counts:{}, do.genes:{}, do.mito:{}, do.ribo:{}".format(
+            task_id, tissue, resolution, method, param, project, do_counts, do_genes, do_mito, do_ribo))
+
+    task_directory, task_name, results_dir = create_dirs(project, tissue, resolution, method, param)
+    adata = filter_cells(adata, resolution, method, param, is_human, do_counts, do_genes, do_mito, do_ribo)
+    adata, marker_dict = cluster_data(adata, compute_markers=True, compute_reductions=True, resolution=resolution)
+    adata = add_cd_scores(adata, is_human)  # add cell death scores
+
+    # write the results
+    write_markers(marker_dict, results_dir)
+    save_to_csv(adata, results_dir)
+
+    # launch R plot script
+    print(subprocess.check_output("Rscript r_plots.R {} {} {} {} {}".format(project, tissue, resolution, method, param),
+                                  shell=True).decode('UTF-8'))
+
+
+if __name__ == '__main__':
+    if local:  # for debug outside of cluster
+        proj = input("Project: ").strip()
+        t_id = int(input("Task ID (-1 to input specific tissue): ").strip())
+        if t_id == -1:
+            tiss = input("Tissue: ").strip()
+            mc_main(proj, 0, tissue=tiss)
+        else:
+            mc_main(proj, t_id)
+    else:  # project and task id are provided as commandline args
+        proj = sys.argv[1]
+        t_id = int(sys.argv[2]) - 1
+        mc_main(proj, t_id)
