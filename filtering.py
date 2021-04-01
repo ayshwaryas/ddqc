3bc19ae5e28f7e8553529933b9cdc21585396351
Michael Alperovich <misha.alperovich1@gmail.com>
2021-04-01 18:43:53 -0400

    cleanup part 1

--- filtering.py
+++ filtering.py
@@ -3,9 +3,18 @@ import re
 import numpy as np
 import pegasus as pg
 
-from utils import cluster_data
-
 INF = 10 ** 10  # infinity for cases no filtering is required
+
+
+# function that performs clustering; does dimensional reductions and finds DE genes if specified
+def cluster_data(adata, resolution):
+    pg.log_norm(adata)
+    pg.highly_variable_features(adata, consider_batch=False)
+    pg.pca(adata, random_state=29)
+    pg.neighbors(adata, K=20, random_state=29)
+    pg.louvain(adata, resolution=resolution, random_state=29)
+
+    return adata
 
 
 # MAD for numpy array. Constant is to make it behave similar to seurat MAD
@@ -47,8 +56,7 @@ def initial_qc(adata, n_genes, percent_mito, mito_prefix, ribo_prefix):
 # metric name - name of the metric (must be in adata.obs)
 # do_upper_co and do_lower_co - whether to do upper and lower cutoff
 # record_path - path for recording filtered cells CSVs (keep it None if not needed)
-def metric_filter(adata, method, param, metric_name, do_lower_co=False, do_upper_co=False, lower_bound=INF, upper_bound=-INF,
-                  record_path=None):
+def metric_filter(adata, method, param, metric_name, do_lower_co=False, do_upper_co=False, record_path=None):
     adata.obs[metric_name + "_qc_pass"] = False  # T/F array to tell whether the cell is filtered
     adata.obs[metric_name + "_lower_co"] = None  # array recording lower cutoff for cell (if exists)
     adata.obs[metric_name + "_upper_co"] = None  # array recording upper cutoff for cell (if exists)
@@ -59,16 +67,16 @@ def metric_filter(adata, method, param, metric_name, do_lower_co=False, do_upper
 
         if method == "mad":  # calculate MAD cutoffs, which are median ± param * MAD
             if do_lower_co:
-                lower_co = min(np.median(cluster.obs[metric_name]) - param * mad(cluster.obs[metric_name]), lower_bound)
+                lower_co = np.median(cluster.obs[metric_name]) - param * mad(cluster.obs[metric_name])
             if do_upper_co:
-                upper_co = max(np.median(cluster.obs[metric_name]) + param * mad(cluster.obs[metric_name]), upper_bound)
+                upper_co = np.median(cluster.obs[metric_name]) + param * mad(cluster.obs[metric_name])
 
         if method == "outlier":  # calculate Outlier cutoffs, which are Q1 - 1.5 * IQR or Q3 + 1.5 * IQR
             q75, q25 = np.percentile(cluster.obs[metric_name], [75, 25])
             if do_lower_co:
-                lower_co = min(q25 - 1.5 * (q75 - q25), lower_bound)
+                lower_co = q25 - 1.5 * (q75 - q25)
             if do_upper_co:
-                upper_co = max(q75 + 1.5 * (q75 - q25), upper_bound)
+                upper_co = q75 + 1.5 * (q75 - q25)
 
         if method == "cutoff":  # cutoff uses the following filtering criteria: >= 200 genes, < 10% mito
             # for comparison only
@@ -105,18 +113,15 @@ def metric_filter(adata, method, param, metric_name, do_lower_co=False, do_upper
 # threshold - parameter for the selected method
 # do_metric - set to true, if you want to filter the data based on metric
 # record_path - path for recording filtered cells CSVs (keep it None if not needed)
-def filter_cells(adata, res, method, threshold, basic_n_genes=100, basic_percent_mito=80, mito_prefix="MT-",
-                 ribo_prefix="^Rp[sl]\d", do_counts=True, do_genes=True, do_mito=True, do_ribo=False, record_path=None,
-                 n_genes_lower_bound=200, percent_mito_upper_bound=10):
+def filter_cells(adata, res=1.3, method="mad", threshold=2, basic_n_genes=100, basic_percent_mito=80, mito_prefix="MT-",
+                 ribo_prefix="^Rp[sl]\d", do_counts=True, do_genes=True, do_mito=True, do_ribo=True, record_path=None):
     adata = initial_qc(adata, basic_n_genes, basic_percent_mito, mito_prefix,
-                       ribo_prefix)  # perform initial qc
-
-    if method == "none":  # no filtering option
-        assert len(adata.obs.index) > 0
-        return adata
-
+                       ribo_prefix)  # perform initial qc with min 100 genes and min 3 cells
     adata_copy = adata.copy()  # make a copy of adata, so the clustering results won't affect future downstream analysis
     adata_copy = cluster_data(adata_copy, res)  # do initial clustering of adata
+
+    if method == "none":  # no filtering option
+        return adata
 
     # for each metric if do_metric is true, the filtering will be performed
     if do_counts:
@@ -124,12 +129,11 @@ def filter_cells(adata, res, method, threshold, basic_n_genes=100, basic_percent
     else:
         adata_copy.obs["n_counts_qc_pass"] = True
     if do_genes:
-        adata_copy = metric_filter(adata_copy, method, threshold, "n_genes", do_lower_co=True, lower_bound=n_genes_lower_bound,
-                                   record_path=record_path)
+        adata_copy = metric_filter(adata_copy, method, threshold, "n_genes", do_lower_co=True, record_path=record_path)
     else:
         adata_copy.obs["n_genes_qc_pass"] = True
     if do_mito:
-        adata_copy = metric_filter(adata_copy, method, threshold, "percent_mito", do_upper_co=True, upper_bound=percent_mito_upper_bound,
+        adata_copy = metric_filter(adata_copy, method, threshold, "percent_mito", do_upper_co=True,
                                    record_path=record_path)
     else:
         adata_copy.obs["percent_mito_qc_pass"] = True
@@ -151,6 +155,4 @@ def filter_cells(adata, res, method, threshold, basic_n_genes=100, basic_percent
 
     adata.obs["passed_qc"] = adata_copy.obs.passed_qc  # transfer array from the copy to actual object
     pg.filter_data(adata)  # perform filtering
-
-    assert len(adata.obs.index) > 0
     return adata
--- run_jc_old.py
+++
@@ -1,7 +0,0 @@
-import sys
-
-from joint_clustering_old import joint_main
-
-project = sys.argv[1]
-tissue = sys.argv[2]
-joint_main(project, 0, tissue=tissue)
