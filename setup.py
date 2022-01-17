1e1ef4f56d1fd5415e9af29062c46205a29fa1ef
Michael Alperovich <misha.alperovich1@gmail.com>
2022-01-16 21:13:29 -0500

    fixed tutorial notebook and increased version number

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 setup(
     name='ddqc',
-    version='0.1.1',
+    version='0.2.0',
     description='Description',
     url='https://github.com/ayshwaryas/ddqc',
     author='Author',
